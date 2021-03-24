4e18b2d254cd5a544f562147021b3a0fdfe1cdde
tadklimp <tadklimp@gmail.com>
2021-03-24 01:31:48 +0100

    Flask test for Spacy = working; not sure if it's the correct way. Unsuccesful attempt for Adjectives to OSC msg.

--- NLTK/liblo_prosodic_OSC.py
+++ NLTK/liblo_prosodic_OSC.py
@@ -1,7 +1,10 @@
 import os
 import prosodic
+import spacy
 import liblo, sys
 from split_sent import split_into_sentences
+from teach_spacy_adj import collect_adjectives
+from teach_spacy_adj import load_model
 
 # setup OSC connection to sclang (57120)
 try:
@@ -10,15 +13,25 @@ except liblo.AddressError as err:
     print(err)
     sys.exit()
 
+# nlp = spacy.load("en_core_web_md")
 
 # create the OSC_messages (empty placeholders)
 osc_sylab_length = liblo.Message("/sylab/length")
 osc_sylab_stress = liblo.Message("/sylab/stress")
 osc_sylab_weight = liblo.Message("/sylab/weight")
 osc_sylab_text = liblo.Message("/sylab/text")
+osc_adjectives = liblo.Message("/sylab/adj")
 
 # collect all syllables in a list
 collect_sylls = []
+
+# extract adjectives and add them to osc list
+# PROBLEM HERE!
+def adjectives_to_osc(text):
+    separator = "Rest(0)"
+    adjs = collect_adjectives(text, separator)
+    print(list(adjs))
+    # osc_adjectives.add( adjs )
 
 # receive text and extract meter with prosodic
 def extract_meter(text):
@@ -46,18 +59,18 @@ def prosodic_labels(text):
 # a simple way to define a sentence-end:
 # insert the "Rest(0)" string at the end of each sentence (in the OSC_message)
 def insert_break():
-        osc_sylab_length.add("Rest(0)")
-        osc_sylab_stress.add("Rest(0)")
-        osc_sylab_weight.add("Rest(0)")
+    identifier = "Rest(0)"
+    osc_sylab_length.add(identifier )
+    osc_sylab_stress.add(identifier )
+    osc_sylab_weight.add(identifier )
 
 
 
 # put it all together and send to Supercollider-sclang
 def meter_to_sclang(text):
     extract_meter(text)
-    liblo.send(target,liblo.Bundle(osc_sylab_length, osc_sylab_stress, osc_sylab_weight, osc_sylab_text)) 
-
-
+    adjectives_to_osc(text)
+    liblo.send(target,liblo.Bundle(osc_sylab_length, osc_sylab_stress, osc_sylab_weight, osc_sylab_text, osc_adjectives)) 
 
 
 
---
+++ NLTK/teach_spacy_adj_Flask.py
@@ -0,0 +1,72 @@
+# source code mostly from here:
+# https://stackoverflow.com/questions/28575082/classify-a-noun-into-abstract-or-concrete-using-nltk-or-similar
+
+from flask import Flask, Response, request
+import spacy
+from spacy_syllables import SpacySyllables
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+
+# added Spacy Syllables
+# syllables = SpacySyllables(nlp)
+# nlp.add_pipe('syllables', after='tagger')
+# print("Pipeline:", nlp.pipe_names)
+nlp=None
+def load_model():
+    global nlp
+    nlp = spacy.load("en_core_web_md")
+
+load_model()
+
+
+# Flask tests
+app = Flask(__name__)
+
+@app.route('/demo', methods=['GET', 'POST'])
+def demo():
+    get_txt = request.get_data(as_text=True)
+    txt = nlp(get_txt)
+    all_adjs = collect_adjectives(txt,"da")
+    return ' '.join(str(e) for e in all_adjs)
+
+adj_classes = ['colour', 'size', 'opinion', 'quantity', 'texture', 'age']
+adj_train_set = [
+    ['red', 'blue', 'white', 'purple', 'green', 'yellow', 'black', 'turquoise', 'magenta', 'pink'],
+    ['big', 'small', 'large', 'huge', 'tiny', 'extensive', 'miniscule', 'long', 'short'] ,
+    ['beautiful', 'ugly', 'real', 'true', 'false', 'perfect', 'interesting', 'good', 'bad', 'costly', 'dangerous', 'tricky', 'disgusting', 'tasty', 'smelly', 'cheap', 'boring', 'easy', 'difficult', 'annoying', 'soothing', 'relaxing', 'sleepy', 'shocking', 'surprising', 'expected', 'unacceptable'],
+    ['many', 'lot', 'much', 'few', 'none', 'all', 'some', 'two', 'ten', 'hundred', 'thousands', 'million'],
+    ['woolen', 'metallic', 'wooden', 'solid', 'soft', 'hard', 'grainy', 'brittle', 'smooth' ],
+    ['new', 'old', 'ancient', 'future', 'past', 'current', 'long time', 'short time', 'sudden', 'immediate']
+]
+
+
+X = np.stack([list(nlp(w))[0].vector for part in adj_train_set for w in part])
+y = [label for label, part in enumerate(adj_train_set) for _ in part]
+classifier = LogisticRegression(C=0.1, class_weight='balanced', solver='lbfgs', multi_class='auto').fit(X, y)
+
+
+# collect all [adjective, category] in an array
+def collect_adjectives(text, separator):
+    # nlp_text = nlp(text)
+    collection = []
+    for token in text:
+        if token.pos_ == 'ADJ':
+            collection.append(token)
+            collection.append(adj_classes[classifier.predict([token.vector])[0]])
+            collection.append(separator)
+    # print(collection)
+    return collection
+
+
+
+# load_model()
+app.run(port = 5000, debug=True, threaded=True) 
+
+# if __name__ == "__main__":
+    # text_raw = """a short text proportional proportional attitude."""
+    # text_raw = "a beautiful big tree was holding many mangos from its turquoise leaves"
+    # text_raw = nlp("some of them will hold a brown book, which i find pedantic in its miniscule content. She asked for a white t-shirt, that was bleached into colourful smelly arrogance. Then the thick sharp edge of the knife glimmed onto her face, bursts into unexpected pie of gooey substance, that stood there with a sudden thump")
+
+
+
+
